4eb7aa071161f5660af5666e2e7db7495577d6a9
François Daoust <fd@tidoust.net>
2017-08-23 09:23:10 +0200

    Reformat JS files to use more modern array constructs (#38)
    Indentation also used a mix of tabulations and spaces. Moved back to 2 spaces
    everywhere.
    
    Also, there was a minor issue with the selector of header content that made it
    duplicate links in the header if there were such links.

--- js/generate-index.js
+++ js/generate-index.js
@@ -1,60 +1,57 @@
-var hero = [];
-var heroOrig = document.querySelectorAll("header *");
-for (var i = 0; i < heroOrig.length; i++) {
-    hero.push(heroOrig[i].cloneNode(true));
-}
-var scripts = ['../js/sidenav.js'];
+const $ = (el, selector) =>
+  Array.prototype.slice.call(el.querySelectorAll(selector), 0);
 
-var templateItem = "<a href=''><div class='icon'><img src='' width='45' alt=''></div><div class='description'><h2></h2><p></p></div></a>";
-var templateTocItem = "<a href=''><div class='description'></div></a>";
+const hero = $(document, 'header > *').map(el => el.cloneNode(true));
 
+const scripts = ['../js/sidenav.js'];
 
-var templateXhr = new XMLHttpRequest();
-var tocXhr = new XMLHttpRequest();
+const templateItem = '<a href=""><div class="icon"><img src="" width="45" alt=""></div><div class="description"><h2></h2><p></p></div></a>';
+const templateTocItem = '<a href=""><div class="description"></div></a>';
+
+let templateXhr = new XMLHttpRequest();
 templateXhr.responseType = 'text';
-templateXhr.open("GET", "../js/template-index");
+templateXhr.open('GET', '../js/template-index');
 templateXhr.onload = function() {
-    document.documentElement.innerHTML = this.responseText;
+  document.documentElement.innerHTML = this.responseText;
+  hero.forEach(el => document.querySelector('.hero .container').appendChild(el));
 
-    for (var i = 0 ; i < hero.length ; i++) {
-        document.querySelector('.hero .container').appendChild(hero[i]);
-    }
-    tocXhr.open("GET", "toc.json");
+  let tocXhr = new XMLHttpRequest();
+  tocXhr.open('GET', 'toc.json');
     tocXhr.onload = function() {
-        var toc = JSON.parse(this.responseText);
-        var ul = document.querySelector("ul.roadmap-list");
+      let toc = JSON.parse(this.responseText);
+      let ul = document.querySelector('ul.roadmap-list');
+      document.querySelector('title').textContent = toc.title;
 
-        document.querySelector('title').textContent = toc.title;
-        var discourseLinks = document.querySelectorAll('section.contribute .discourse');
-        for (var i = 0 ; i < discourseLinks.length; i++) {
-            discourseLinks[i].href = toc.discourse.url;
-            if (discourseLinks[i].classList.contains('discoursecat')) {
-                discourseLinks[i].textContent = toc.discourse.category;
-            }
+      $(document, 'section.contribute .discourse').forEach(link => {
+        link.href = toc.discourse.url;
+        if (link.classList.contains('discoursecat')) {
+            link.textContent = toc.discourse.category;
         }
+      });
 
-        var nav = document.querySelector("aside nav ul");
-        for (var i = 0 ; i < toc.pages.length; i++) {
-            var li = document.createElement("li");
-            li.innerHTML = templateItem;
-            li.querySelector("a").href = toc.pages[i].url;
-            li.querySelector("h2").textContent = toc.pages[i].title;
-            li.querySelector("img").src = toc.pages[i].icon;
-            li.querySelector("p").textContent = toc.pages[i].description;
-            ul.appendChild(li);
+      let nav = document.querySelector('aside nav ul');
+      toc.pages.forEach(page => {
+        let li = document.createElement('li');
+        li.innerHTML = templateItem;
+        li.querySelector('a').href = page.url;
+        li.querySelector('h2').textContent = page.title;
+        li.querySelector('img').src = page.icon;
+        li.querySelector('p').textContent = page.description;
+        ul.appendChild(li);
 
-            var navLi = document.createElement("li");
-            navLi.innerHTML = templateTocItem;
-            navLi.querySelector("a").href = toc.pages[i].url;
-            navLi.querySelector("div.description").textContent = toc.pages[i].title;
-            nav.appendChild(navLi);
-        }
-    }
+        let navLi = document.createElement('li');
+        navLi.innerHTML = templateTocItem;
+        navLi.querySelector('a').href = page.url;
+        navLi.querySelector('div.description').textContent = page.title;
+        nav.appendChild(navLi);
+      });
+    };
     tocXhr.send();
-    for (var i = 0 ; i < scripts.length ; i++) {
-        var s = document.createElement("script");
-        s.src = scripts[i];
-        document.querySelector('body').appendChild(s);
-    }
+
+    scripts.forEach(script => {
+      let s = document.createElement('script');
+      s.src = script;
+      document.querySelector('body').appendChild(s);
+    });
 }
 templateXhr.send();
