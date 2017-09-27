63295d58777ab126701d94824a94e73c37a7b9d1
Fuqiao Xue <xfq@w3.org>
2017-09-27 17:40:34 +0800

    Resolve the data/data/ issue on gh-pages

--- compile.sh
+++ compile.sh
@@ -9,14 +9,14 @@ python tools/extract-spec-data.py data/*.json > specs/tr.json
 python tools/extract-impl-data.py data/*.json > specs/impl.json
 
 if [ -d out ]; then
-  cp -R assets out/assets/
-  cp -R data out/data/
-  cp -R js out/js/
-  cp -R media out/media/
-  cp -R mobile out/mobile/
-  cp -R publishing out/publishing/
-  cp -R security out/security/
-  cp -R web5g out/web5g/
+  cp -R assets/ out/assets/
+  cp -R data/ out/data/
+  cp -R js/ out/js/
+  cp -R media/ out/media/
+  cp -R mobile/ out/mobile/
+  cp -R publishing/ out/publishing/
+  cp -R security/ out/security/
+  cp -R web5g/ out/web5g/
   cp specs/tr.json out/specs/tr.json
   cp specs/impl.json out/specs/impl.json
 fi
