287f1b553fc08a10c7e87e5218b57d14d48066a3
Fuqiao Xue <xfq@w3.org>
2017-09-27 19:14:19 +0800

    Fixing my previous fix...

--- compile.sh
+++ compile.sh
@@ -9,14 +9,14 @@ python tools/extract-spec-data.py data/*.json > specs/tr.json
 python tools/extract-impl-data.py data/*.json > specs/impl.json
 
 if [ -d out ]; then
-  cp -R assets/ out/assets/
-  cp -R data/ out/data/
-  cp -R js/ out/js/
-  cp -R media/ out/media/
-  cp -R mobile/ out/mobile/
-  cp -R publishing/ out/publishing/
-  cp -R security/ out/security/
-  cp -R web5g/ out/web5g/
+  rm -rf out/assets && cp -R assets/ out/assets/
+  rm -rf out/data && cp -R data/ out/data/
+  rm -rf out/js && cp -R js/ out/js/
+  rm -rf out/media && cp -R media/ out/media/
+  rm -rf out/mobile && cp -R mobile/ out/mobile/
+  rm -rf out/publishing && cp -R publishing/ out/publishing/
+  rm -rf out/security && cp -R security/ out/security/
+  rm -rf out/web5g && cp -R web5g/ out/web5g/
   cp specs/tr.json out/specs/tr.json
   cp specs/impl.json out/specs/impl.json
 fi
--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,8 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-# rm -rf out/**/* || exit 0
-rm -rf out/* || exit 0
+# rm -rf out/* || exit 0
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
